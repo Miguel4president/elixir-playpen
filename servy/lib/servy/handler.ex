fae861956a4e33845657358856a4d96af8661f20
Aaron Votre <avotre422@gmail.com>
2017-06-23 17:06:42 -0400

    Add subpath matching
    Omg, you can match using string concat...swoon

--- servy/lib/servy/handler.ex
+++ servy/lib/servy/handler.ex
@@ -14,7 +14,11 @@ defmodule Servy.Handler do
       |> List.first    
       |> String.split(" ")
 
-    %{ method: method, path: path, resp_body: "" }
+    %{ method: method, 
+       path: path, 
+       resp_body: "",
+       status: nil
+     }
   end
 
   def route(%{method: "GET", path: "/wildthings"} = conv) do
@@ -25,18 +29,37 @@ defmodule Servy.Handler do
     %{ conv | resp_body: "Teddy, Smokey, Paddington" }    
   end
 
+  def route(%{method: "GET", path: "/bears/" <> id} = conv) do
+    %{ conv | status: 200, resp_body: "Bear #{id}" }
+  end
+
+  def route(conv) do
+    %{ conv | status: 404, resp_body: "No #{conv.path} here!"}
+  end
+
   def format_response(conv) do
-    # TODO: Use values in the map to create an HTTP response string:
     """
-    HTTP/1.1 200 OK
+    HTTP/1.1 #{conv.status} #{status_reason(conv.status)}
     Content-Type: text/html
-    Content-Length: #{byte_size(conv.resp_body)}
+    Content-Length: #{String.length(conv.resp_body)}
 
     #{conv.resp_body}
     """
   end
+
+  defp status_reason(code) do
+    %{
+      200 => "OK",
+      201 => "Created",
+      401 => "Unauthorized",
+      403 => "Forbidden",
+      404 => "Not Found",
+      500 => "Internal Server Error"
+    }[code]
+  end
   
 end
+
 
 request = """
 GET /wildthings HTTP/1.1
@@ -46,7 +69,11 @@ Accept: */*
 
 """
 
-request2 = """
+response = Servy.Handler.handle(request)
+
+IO.puts response
+
+request = """
 GET /bears HTTP/1.1
 Host: example.com
 User-Agent: ExampleBrowser/1.0
@@ -54,14 +81,29 @@ Accept: */*
 
 """
 
-request3 = """
-GET /wildthings HTTP/1.1
+response = Servy.Handler.handle(request)
+
+IO.puts response
+
+request = """
+GET /bigfoot HTTP/1.1
 Host: example.com
 User-Agent: ExampleBrowser/1.0
 Accept: */*
 
 """
 
+response = Servy.Handler.handle(request)
+
+IO.puts response
+
+request = """
+GET /bears/1 HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
 
 response = Servy.Handler.handle(request)
 
