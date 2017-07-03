0bee1bb1e1ba8d20c677f50832b410dba24f41ca
Aaron Votre <avotre422@gmail.com>
2017-07-03 15:20:03 -0400

    (#16) Comprehensions and templates

--- servy/lib/servy/bear_controller.ex
+++ servy/lib/servy/bear_controller.ex
@@ -2,35 +2,36 @@ defmodule Servy.BearController do
   
   alias Servy.Wildthings
   alias Servy.Bear
+  alias Servy.BearView
 
-  defp bear_item(bear) do
-    "<li>#{bear.name} - #{bear.type}</li>"
-  end
+  @templates_path Path.expand("../../templates", __DIR__)
 
   def index(conv) do
-    items = 
-      Wildthings.list_bears()
-      |> Enum.filter(&Bear.is_grizzly/1)
-      |> Enum.sort(&Bear.order_asc_by_name/2)
-      |> Enum.map(&bear_item/1)
-      |> Enum.join
+  bears =
+    Wildthings.list_bears()
+    |> Enum.sort(&Bear.order_asc_by_name/2)
 
-    %{ conv | status: 200, resp_body: "<ul>#{items}</ul>" }    
-  end
+  %{ conv | status: 200, resp_body: BearView.index(bears) }
+end
 
-  def show(conv, %{"id" => id}) do
-    bear = Wildthings.get_bear(id)
+def show(conv, %{"id" => id}) do
+  bear = Wildthings.get_bear(id)
 
-    %{ conv | status: 200, resp_body: "<h1>Bear #{bear.id}: #{bear.name}</h1>" }
-  end
+  %{ conv | status: 200, resp_body: BearView.show(bear) }
+end
 
   def create(conv, %{"name" => name, "type" => type}) do
     %{ conv | status: 201, 
               resp_body: "Created a #{type} bear named #{name}!" }
   end
 
-  def delete(conv, _params) do
-    %{ conv | status: 403, resp_body: "Bears must never be deleted!"}
-  end
+  # defp render(conv, template, bindings \\ []) do
+  #   content =
+  #     @templates_path
+  #     |> Path.join(template)
+  #     |> EEx.eval_file(bindings)
+
+  #   %{ conv | status: 200, resp_body: content }
+  # end
 
 end
